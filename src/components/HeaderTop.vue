e8d2c26c5dfd5fe388e4490ed31bf15d5b49952a
emil584c <105641442+emil584c@users.noreply.github.com>
2023-04-27 13:28:35 +0200

    setup

--- src/components/HeaderTop.vue
+++ src/components/HeaderTop.vue
@@ -5,4 +5,9 @@
 <template>
 
     
+    <div class="flex gap-2 items-center mt-6 mx-auto justify-center group">
+        <p class="text-3xl text-center duration-300 cursor-pointer group-hover:tracking-widest">Carlsens Kvarter</p>
+        <img class="h-8 opacity-0 group-hover:opacity-100 duration-500 relative right-8 group-hover:right-1" src="https://cdn-icons-png.flaticon.com/512/3069/3069352.png" alt="">
+    </div>
+
 </template>
