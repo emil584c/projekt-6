3603a02779dd9c95f06347805c3b7f613b33116d
emil584c <105641442+emil584c@users.noreply.github.com>
2023-05-08 10:44:31 +0200

    components setup

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,20 @@
 <script setup>
-
+import HeaderTop from '../components/HeaderTop.vue'
+import AboutHero from '../components/AboutHero.vue'
+import TeamSection from '../components/TeamSection.vue'
+import HistorySection from '../components/TeamSection.vue'
+import SocialMedia from '../components/SocialMedia.vue'
+import FooterBottom from '../components/FooterBottom.vue'
 </script>
 
 <template>
+
+<HeaderTop />
+<AboutHero />
+<TeamSection />
+<HistorySection />
+<SocialMedia />
+<FooterBottom />
 
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup>
 import HeaderTop from '../components/HeaderTop.vue'
+import HomepageHero from '../components/HomepageHero.vue'
+import AboutFrontpage from '../components/AboutFrontpage.vue'
+import ProductSection from '../components/ProductSection.vue'
+import SocialMedia from '../components/SocialMedia.vue'
+import FooterBottom from '../components/FooterBottom.vue'
 </script>
 
 <template>
 
 <HeaderTop />
+<HomepageHero />
+<AboutFrontpage />
+<ProductSection />
+<SocialMedia />
+<FooterBottom />
+
 
 </template>
