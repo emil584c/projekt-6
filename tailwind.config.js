b91e73eba17de5cb03f7277ce12c22f02aac1eb2
emil584c <105641442+emil584c@users.noreply.github.com>
2023-05-08 23:04:40 +0200

    color setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cream-primary': '#243c5a',
+        'brown-primary': '#411f1e',
+        'green-primary': '#2e5342'
+      },
+      maxWidth: {
+        'content-width': '1166px',
+      }
+    },
   },
   plugins: [],
 }
