d763e24f7373e545e6ef68a60615ed65cab932bd
emil584c <105641442+emil584c@users.noreply.github.com>
2023-05-10 10:14:08 +0200

    Contact page setup

---
+++ src/views/ContactView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HeaderTop from '../components/HeaderTop.vue'
+import ContactHero from '../components/ContactHero.vue'
+import ContactSection from '../components/ContactSection.vue'
+import SocialMedia from '../components/SocialMedia.vue'
+import FooterBottom from '../components/FooterBottom.vue'
+</script>
+
+<template>
+<HeaderTop />
+<ContactHero />
+<ContactSection />
+<SocialMedia />
+<FooterBottom />
+
+</template>
