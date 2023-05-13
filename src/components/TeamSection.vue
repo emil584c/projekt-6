b0063aeb6f5b8e08dafa912632b4a66d79b651e2
emil584c <105641442+emil584c@users.noreply.github.com>
2023-05-13 14:23:13 +0200

    team section setup

--- src/components/TeamSection.vue
+++ src/components/TeamSection.vue
@@ -5,17 +5,20 @@
 </script>
 
 <template>
-    <section>
+    <section class="bg-cream-primary">
         <div class="max-w-sm px-4 md:max-w-content-width mx-auto">
             <h2>MØD HOLDET BAG BAREN</h2>
-            <p class=" text-black-primary">Vores personale står klar til enhver tid at give dig en personlig betjening. Herunder ser du vores faste personale som du kan være sikker på at møde i baren når du kommer.</p>
+            <p class=" text-black-primary max-w-[685px] mb-4">Vores personale står klar til enhver tid at give dig en personlig betjening. Herunder ser du vores faste personale som du kan være sikker på at møde i baren når du kommer.</p>
        
 
-            <div class=" w-full grid grid-cols-4">
-                <div class="w-[261px] h-[410px] bg-brown-primary"></div>
-                <div class="w-[261px] h-[410px] bg-brown-primary"></div>
-                <div class="w-[261px] h-[410px] bg-brown-primary"></div>
-                <div class="w-[261px] h-[410px] bg-brown-primary"></div>
+            <div class=" flex flex-wrap w-full justify-center md:grid md:grid-cols-4">
+                <div class="w-[261px] h-[410px] bg-brown-primary p-2">
+                    <img class="border-solid border-[2px] border-border-white mb-2" src="../assets/images/placeholders/simonstagefoto.jpg" alt="picture of simon stage">
+                    <div class="w-full">
+                        <p class="team-name mb-[-4px]">Simon Stage</p>
+                        <p class="team-nickname">Bar chef</p>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
