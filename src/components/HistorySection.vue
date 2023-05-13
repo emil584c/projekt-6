1d562eb9781c58c4f7867e625b9bb35e119dfaca
emil584c <105641442+emil584c@users.noreply.github.com>
2023-05-13 14:40:35 +0200

    history section

--- src/components/HistorySection.vue
+++ src/components/HistorySection.vue
@@ -1,7 +1,41 @@
+<!-- Emil Skriver -->
 <script setup>
 
 </script>
 
 <template>
+
+    <section class=" bg-cream-primary pt-12">
+        <div class=" max-w-sm px-4 md:max-w-content-width mx-auto">
+            <h2>VORES HISTORIE</h2>
+            <div class="flex flex-col gap-4 md:gap-10">
+
+               
+                <div class="flex gap-4 flex-col md:gap-10 md:flex-row md:justify-between">
+                    <div class="bg-[url('../assets/images/placeholders/menufoto.jpg')] bg-cover w-mobilebox-width h-mobilebox-height bg-center flex flex-col justify-center gap-6 pl-5 md:w-box-width"></div>
+                    <div class=" bg-green-primary w-mobilebox-width h-fit md:h-mobilebox-height bg-center flex flex-col justify-center p-[10px] md:w-full md:max-w-longbox-width">
+                
+                        <div class="border-solid border-[1px] border-border-white w-full h-full p-6 flex flex-col justify-center relative">
+                            <h3>HUSETS OPRINDELSE</h3>
+                            <p class=" mb-3 max-w-[386px] md:max-w-[600px]">Huset, som nu er hjemsted for vores hyggelige bar, blev bygget i 1898 og var oprindeligt Albani Apotek, med et smukt loft lavet af opal glas.</p>
+                            <p class=" max-w-[386px] md:max-w-[600px]">Efter at apoteket flyttede i 1963, blev lokalerne brugt til alt fra tegnemaskiner til lystryk og fotokopier for firmaer. Men i 1972 blev Carlsens Kvarter åbnet som værtshus, takket være den kendte restauratør Henning Carlsen. Han var en mand med visioner og ønskede, at hans værtshus skulle være et 'pænt' sted, så han satte nogle faste regler, herunder ingen servering fra baren.</p>
+                        </div>
+                     </div>
+                </div>
+
+                <div class="flex gap-4 flex-col-reverse md:gap-10 md:flex-row md:justify-between">
+                    <div class=" bg-brown-primary w-mobilebox-width h-fit md:h-mobilebox-height bg-center flex flex-col justify-center p-[10px] md:w-full md:max-w-longbox-width">
+                        <div class="border-solid border-[1px] border-border-white w-full h-full p-6 flex flex-col justify-center relative">
+                            <h3>CARLSENS KVARTERS REJSE</h3>
+                            <p class=" mb-3 max-w-[386px] md:max-w-[600px]">Efter Henning Carlsens bortgåen blev stedet overtaget af hans tre børn, som senere solgte det til John Milton. Benny Weble overtog det i 1991 og fortsatte Carlsens Kvarters tradition med at være et sted, hvor hygge og god service var i højsædet.</p>
+                            <p class=" max-w-[386px] md:max-w-[600px]">Vi er stolte af vores historie og fortsætter med at tilbyde vores gæster det bedste af det bedste. I 1997 introducerede vi vores første tre egen importerede belgiske øl. Vi har siden udvidet vores sortiment, men vores mål er stadig det samme - at tilbyde vores gæster en varm og hyggelig oplevelse med god og personlig service.</p>
+                        </div>
+                     </div>
+                     <div class="bg-[url('../assets/images/placeholders/menufoto.jpg')] bg-cover w-mobilebox-width h-mobilebox-height bg-center flex flex-col justify-center gap-6 pl-5 md:w-box-width"></div>
+                </div>
+
+            </div>
+        </div>
+    </section>
 
 </template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,7 @@
 import HeaderTop from '../components/HeaderTop.vue'
 import AboutHero from '../components/AboutHero.vue'
 import TeamSection from '../components/TeamSection.vue'
-import HistorySection from '../components/TeamSection.vue'
+import HistorySection from '../components/HistorySection.vue'
 import SocialMedia from '../components/SocialMedia.vue'
 import FooterBottom from '../components/FooterBottom.vue'
 </script>
