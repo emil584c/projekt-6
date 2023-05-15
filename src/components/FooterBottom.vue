da79579495fe13de8f6eec69b1f05c181ba7d021
IBATMANI <abasinsafi@hotmail.com>
2023-05-15 14:38:23 +0200

    footer desktop update

--- src/components/FooterBottom.vue
+++ src/components/FooterBottom.vue
@@ -5,27 +5,36 @@
 
 <template>
 
-    <header class=" bg-brown-primary">
-        <div class= "flex flex-col items-start justify-center py-7 max-w-[330px] mx-auto">
-            <img class="justify-item-start" src="../assets/images/placeholders/cklogo.svg" alt="carlsens logo">
-            <div class="bg-border-white w-full md:max-w-content-width h-[2px] my-2"></div>
-            <p>Kom forbi Carlsens Kvarter, og få en iskold fadøl på Odenses hyggeligste ølbar. Vi garanterer en unik smagsoplevelse og højt humør på alle dagens tidspunkter.</p>
+    <footer class=" bg-brown-primary">
+        <div class= "flex flex-col items-start justify-center py-8 max-w-[330px] mx-auto md:flex-row md:max-w-10xl">
             
-            <h2 class="text-border-white mt-10 mb-2">INFORMATION</h2>
-            <div class="mb-1">
-                <p>Hunderupvej 19, 5000 Odense C</p>
-                <p>+45 63 11 01 10</p>
-                <p>carlsens@carlsens.dk</p>
-            </div>
+                <div class="md:flex-col md:mr-32">
+                    <img class="justify-item-start" src="../assets/images/placeholders/cklogo.svg" alt="carlsens logo">
+                    <div class="bg-border-white w-full md:max-w-content-width h-[2px] my-2"></div>
+                    <p>Kom forbi Carlsens Kvarter, og få en iskold fadøl på Odenses hyggeligste ølbar. Vi garanterer en unik smagsoplevelse og højt humør på alle dagens tidspunkter.</p>
+                </div>
+
+                <div class="flex-col md:mr-32">
+                    <h2 class="text-border-white mt-10 mb-2">INFORMATION</h2>
+                        <div class="mb-1">
+                        <p>Hunderupvej 19, 5000 Odense C</p>
+                        <p>+45 63 11 01 10</p>
+                        <p>carlsens@carlsens.dk</p>
+                    </div>
+                </div>
+                
+                <div class="flex-col md:mr-32">
+                    <h2 class="text-border-white mt-9 mb-2">ÅBNINGSTIDER</h2>
+                    <div class="mb-1">
+                        <p>Man - Lør: 12:00 - 01:00</p>
+                        <p>Søndag: 13:00 - 19:00</p>
+                    </div>
+                </div>
             
-            <h2 class="text-border-white mt-9 mb-2">ÅBNINGSTIDER</h2>
-            <div class="mb-1">
-                <p>Man - Lør: 12:00 - 01:00</p>
-                <p>Søndag: 13:00 - 19:00</p>
-            </div>
+
             <div class="bg-border-white w-full max-w-content-width h-[2px]"></div>
             <p class="mt-4">© 2023 Carlsens Kvarter</p>
         </div>
-    </header>
+    </footer>
     
 </template>
