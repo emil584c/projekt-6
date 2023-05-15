2b5c62f3aafa31d396a203044ecad91ce0edb2b2
IBATMANI <abasinsafi@hotmail.com>
2023-05-15 14:52:10 +0200

    margin footer desktop

--- src/components/FooterBottom.vue
+++ src/components/FooterBottom.vue
@@ -14,18 +14,28 @@
                     <p>Kom forbi Carlsens Kvarter, og få en iskold fadøl på Odenses hyggeligste ølbar. Vi garanterer en unik smagsoplevelse og højt humør på alle dagens tidspunkter.</p>
                 </div>
 
-                <div class="flex-col md:mr-32">
-                    <h2 class="text-border-white mt-10 mb-2">INFORMATION</h2>
-                        <div class="mb-1">
-                        <p>Hunderupvej 19, 5000 Odense C</p>
-                        <p>+45 63 11 01 10</p>
-                        <p>carlsens@carlsens.dk</p>
+                <div class="md:flex-col md:mr-32">
+                    <h2 class="text-border-white mt-12 mb-4.5">NYTTIGE LINKS</h2>
+                    <div class="mb-3">
+                        <router-link to="/"><p>Forside</p></router-link>
+                        <router-link to="/"><p>Vi tilbyder</p></router-link>
+                        <router-link to="/about"><p>Hvem er vi</p></router-link>
+                        <router-link to="/contact"><p>Kontakt os</p></router-link>
                     </div>
                 </div>
-                
+
                 <div class="flex-col md:mr-32">
-                    <h2 class="text-border-white mt-9 mb-2">ÅBNINGSTIDER</h2>
-                    <div class="mb-1">
+                    <h2 class="text-border-white mt-12 mb-4.5">INFORMATION</h2>
+                        <div class="mb-3">
+                            <p>Hunderupvej 19, 5000 Odense C</p>
+                            <p>+45 63 11 01 10</p>
+                            <p>carlsens@carlsens.dk</p>
+                        </div>
+                </div>
+                
+                <div class="flex-col">
+                    <h2 class="text-border-white mt-12 mb-4.5">ÅBNINGSTIDER</h2>
+                    <div class="mb-3">
                         <p>Man - Lør: 12:00 - 01:00</p>
                         <p>Søndag: 13:00 - 19:00</p>
                     </div>
