99f6250f9a8619083b0ccc1586cb1f7e96b0f497
IBATMANI <abasinsafi@hotmail.com>
2023-05-15 12:52:51 +0200

    footer margin

--- src/components/FooterBottom.vue
+++ src/components/FooterBottom.vue
@@ -11,12 +11,17 @@
             <div class="bg-border-white w-full md:max-w-content-width h-[2px] "></div>
             <p>Kom forbi Carlsens Kvarter, og få en iskold fadøl på Odenses hyggeligste ølbar. Vi garanterer en unik smagsoplevelse og højt humør på alle dagens tidspunkter.</p>
             <h2 class="text-border-white mt-10 mb-2">INFORMATION</h2>
-            <p>Hunderupvej 19, 5000 Odense C</p>
-            <p>+45 63 11 01 10</p>
-            <p>carlsens@carlsens.dk</p>
-            <h2 class="text-border-white mt-10 mb-2">ÅBNINGSTIDER</h2>
-            <p>Man - Lør: 12:00 - 01:00</p>
-            <p>Søndag: 13:00 - 19:00</p>
+            <div class="mb-1">
+                <p>Hunderupvej 19, 5000 Odense C</p>
+                <p>+45 63 11 01 10</p>
+                <p>carlsens@carlsens.dk</p>
+            </div>
+            
+            <h2 class="text-border-white mt-9 mb-2">ÅBNINGSTIDER</h2>
+            <div class="mb-1">
+                <p>Man - Lør: 12:00 - 01:00</p>
+                <p>Søndag: 13:00 - 19:00</p>
+            </div>
             <div class="bg-border-white w-full max-w-content-width h-[2px]"></div>
         </div>
     </header>
