95079ceee5bbf23acfef52aad1322b8f5a23fbf3
emil584c <105641442+emil584c@users.noreply.github.com>
2023-05-15 12:21:41 +0200

    social media start

--- src/components/SocialMedia.vue
+++ src/components/SocialMedia.vue
@@ -1,8 +1,16 @@
+<!-- Emil skriver -->
 <script setup>
 
 </script>
 
 <template>
-
+<section class=" bg-cream-primary">
+    <div class=" max-w-content-width mx-auto flex justify-between">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+</section>
 
 </template>
