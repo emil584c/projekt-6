ab4387b6b8dc1cff343ee708586afc8a991fbf12
emil584c <105641442+emil584c@users.noreply.github.com>
2023-05-15 10:35:28 +0200

    navigation

--- src/components/FrontpageHero.vue
+++ src/components/FrontpageHero.vue
@@ -5,7 +5,7 @@
 
 <template>
 
-<section class=" pt-6 overflow-hidden bg-cream-primary">
+<section class=" pt-6 max-h-[624px] overflow-hidden bg-cream-primary">
     <div class="w-full h-[70vh] relative md:px-6 md:mx-auto md:max-w-content-width flex flex-col items-center">
         <video autoplay muted loop class="h-[70vh] md:h-[80vh] w-full object-cover object-center" src="../assets/hero_video_color.mp4"></video>
         <div class=" bg-cream-primary text-center p-2 relative bottom-48 mx-4 max-w-[578px] md:bottom-60">
