986a46eb1bd5edb38e6e65824eceefbf856f1302
emil584c <105641442+emil584c@users.noreply.github.com>
2023-05-16 21:20:00 +0200

    instagram posts

--- src/components/SocialMedia.vue
+++ src/components/SocialMedia.vue
@@ -4,12 +4,18 @@
 </script>
 
 <template>
-<section class=" bg-cream-primary">
-    <div class=" max-w-content-width mx-auto flex justify-between">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+<section class=" bg-cream-primary py-14">
+    <div class=" mx-auto max-w-content-width">
+        <h2 class=" mb-5">FØLG OS PÅ INSTAGRAM</h2>
+        <div class=" max-w-content-width mx-auto flex justify-between">
+            <a class="h-fit w-fit overflow-hidden" href="https://www.instagram.com/p/CsF0sfwMapP/" target="_blank"><img class="w-[270px] h-[270px] object-cover hover:scale-105 duration-500 hover:opacity-80" src="../assets/images/instagram/post1.png" alt="Carlsens Kvarter instagram post"></a>
+            <a class="h-fit w-fit overflow-hidden" href="https://www.instagram.com/p/CpAvO5toJpF/" target="_blank"><img class="w-[270px] h-[270px] object-cover hover:scale-105 duration-500 hover:opacity-80" src="../assets/images/instagram/post2.png" alt="Carlsens Kvarter instagram post"></a>
+            <a class="h-fit w-fit overflow-hidden" href="https://www.instagram.com/p/CpfPvMOsuAE/" target="_blank"><img class="w-[270px] h-[270px] object-cover hover:scale-105 duration-500 hover:opacity-80" src="../assets/images/instagram/post3.png" alt="Carlsens Kvarter instagram post"></a>
+            <a class="h-fit w-fit overflow-hidden bg-white relative" href="https://www.instagram.com/p/Cmw67mNsD5E/" target="_blank">
+                <img class="w-[270px] h-[270px] object-cover hover:scale-105 duration-500 hover:opacity-80" src="../assets/images/instagram/post4.png" alt="Carlsens Kvarter instagram post">
+                <p class="absolute bottom-2 right-8">BABA</p>
+            </a>
+        </div>
     </div>
 </section>
 
