18743a1ad277beae935960b6398b7674f79df659
IBATMANI <abasinsafi@hotmail.com>
2023-05-17 10:52:09 +0200

    Kontakt side desktop størrelser

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -8,7 +8,7 @@ import buttonPrimary from './ButtonPrimary.vue'
     <section class=" bg-cream-primary">
         <div class="flex-auto flex-col items-start justify-center max-w-[330px] mx-auto md:flex-row md:max-w-content-width ">
             <div class="flex flex-col md:flex-row-reverse md:justify-between">
-                <div class=" bg-green-primary w-mobilebox-width h-mobilelongbox-height md:h-mobilebox-height bg-center flex flex-col justify-center p-[10px] md:w-full md:max-w-longbox-width">
+                <div class=" bg-green-primary w-mobilebox-width h-mobilelongbox-height md:h-box-width bg-center flex flex-col justify-center p-[10px] md:w-full md:max-w-longbox-width">
                     <div class="border-solid border-[1px] border-border-white w-full h-full p-6 flex flex-col justify-center relative">
                             <h3>BESTILLINGER</h3>
                             <p class=" mb-3 max-w-[386px]">Ved bestilling af håndmadder, ølsmagninger eller andre arrangementer foregår bestillingen på vores Facebook Messenger.</p>
@@ -16,24 +16,24 @@ import buttonPrimary from './ButtonPrimary.vue'
                             <buttonPrimary buttonText="BESTIL"/>
                     </div>
                     </div>
-                    <div class="bg-[url('../assets/images/bestillinger_carlsens_kvarter.jpg')] bg-cover my-[20px] md:my-[0px] w-mobilebox-width h-mobilebox-height bg-center flex flex-col justify-center pl-5 md:w-box-width"></div>
+                    <div class="bg-[url('../assets/images/bestillinger_carlsens_kvarter.jpg')] bg-cover my-[20px] md:my-[0px] w-mobilebox-width h-mobilebox-height bg-center flex flex-col justify-center pl-5 md:w-box-width md:h-box-width"></div>
                 </div>
 
-            <div class="flex flex-col md:flex-row md:justify-between">
-                <div class=" bg-brown-primary w-mobilebox-width h-mobilelongbox-height md:h-mobilebox-height bg-center flex flex-col justify-center p-[10px] md:w-full md:max-w-longbox-width">               
+            <div class="flex flex-col md:flex-row md:justify-between md:my-[40px]">
+                <div class=" bg-brown-primary w-mobilebox-width h-mobilelongbox-height md:h-box-width bg-center flex flex-col justify-center p-[10px] md:w-full md:max-w-longbox-width">               
                     <div class="border-solid border-[1px] border-border-white w-full h-full p-6 flex flex-col justify-center ">
                             <h3>SPØRGSMÅL?</h3>
                             <p class=" mb-3 max-w-[386px]">Hvis du har spørgsmål eller forespørgsler udover bestilling af ølsmagning, håndmadder eller arrangement, kan du kontakte os på følgende muligheder.</p>
                             <p class=" text-base font-medium">BORDRESERVATION ER IKKE MULIGT.</p>
                             <div class="bg-border-white w-full md:max-w-content-width h-[1px] my-2"></div>
-                         <div class="md:flex-row">
+                        <div class="md:flex-row">
                             <p class="mb-1 md:mb-3 ">+45 63 11 01 10</p>
                             <p class="mb-1 md:mb-3 ">carlsens@carlsens.dk</p>
                             <p class="mb-1 md:mb-3 ">Hunderupvej 19, 5000 Odense C</p>
                         </div>
                     </div>
                 </div>
-                <div class=" bg-brown-primary my-[20px] md:my-[0px] w-mobilebox-width h-[363] bg-center flex flex-col justify-center p-[10px] md:w-full md:h-mobilebox-height md:max-w-longbox-width">               
+                <div class=" bg-brown-primary my-[20px] w-mobilebox-width h-[363] bg-center flex flex-col justify-center p-[10px] md:my-[0px] md:w-full md:max-w-[363px]">               
                     <div class="border-solid border-[1px] border-border-white w-full h-full p-6 flex flex-col justify-center ">
                         <ul class="flex flex-col gap-1">
                             <h3>ÅBNINGSTIDER</h3>
