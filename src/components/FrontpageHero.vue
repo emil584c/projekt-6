f0c6004270f55caa6e17c95691ff98f3de35ea7c
emil584c <105641442+emil584c@users.noreply.github.com>
2023-05-18 10:05:03 +0200

    hero fix

--- src/components/FrontpageHero.vue
+++ src/components/FrontpageHero.vue
@@ -5,9 +5,9 @@
 
 <template>
 
-<section class=" bg-cream-primary">
-    <div class=" h-[80vh] w-full md:max-w-content-width flex flex-col mx-auto justify-end items-center pb-12 relative overflow-hidden">
-        <video class="h-[80vh] object-cover lg:h-fit absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 scale-105" src="../assets/hero_video_color.mp4" muted autoplay loop></video>
+<section class=" bg-cream-primary pt-4">
+    <div class=" h-[80vh] w-full md:max-w-content-width flex flex-col mx-auto justify-end items-center pb-12 relative overflow-hidden md:h-[624px]">
+        <video class="h-full w-full object-cover lg:h-fit absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 scale-105" src="../assets/hero_video_color.mp4" muted autoplay loop></video>
         <div class=" bg-cream-primary text-center p-2 max-w-[578px] z-20 relative">
             <img class="absolute top-[-4px] left-1/2 transform -translate-x-1/2 -translate-y-1/2" src="../assets/images/logos/ck_logo_round.svg" alt="carlsens kvarters runde logo">
             <div class="w-full h-full border-solid border-[1px] border-border-black px-6 py-5 flex flex-col justify-center items-center">
