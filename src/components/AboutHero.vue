45b7e4f8cd3cda4135f15640af3827f824e62c54
emil584c <105641442+emil584c@users.noreply.github.com>
2023-05-19 10:37:07 +0200

    image fixes

--- src/components/AboutHero.vue
+++ src/components/AboutHero.vue
@@ -5,8 +5,9 @@
 <template>
     <section class="overflow-hidden bg-cream-primary pb-8">
         <div class="w-full relative px-6 flex flex-col items-center md:px-0 md:mx-auto md:max-w-content-width">
-            <img class="h-[60vh] md:h-[80vh] w-full object-cover object-center" src="../assets/images/hvem_er_vi_carlsens_kvarter.jpg"/>
-            <div class=" bg-cream-primary text-center p-2 absolute bottom-10 mx-4 max-w-[578px] md:bottom-40">
+            <img class="hidden md:block md:h-full w-full object-cover object-center" src="../assets/images/hvem_er_vi_carlsens_kvarterstor.jpg"/>
+            <img class=" md:hidden h-[60vh] md:h-full w-full object-cover object-center" src="../assets/images/hvem_er_vi_carlsens_kvarter_mobil.jpg"/>
+            <div class=" bg-cream-primary text-center p-2 absolute bottom-10 mx-4 max-w-[578px]">
                 <img class="hidden" src="" alt="carlsens logo">
                 <div class="w-full h-full border-solid border-[1px] border-border-black px-6 py-5 flex flex-col justify-center items-center md:min-w-[578px]">
                     <h1>HVEM ER VI</h1>
