689d74e0cebf5a5e8e23400000e9d5c1ebfd40b7
emil584c <105641442+emil584c@users.noreply.github.com>
2023-05-19 14:47:40 +0200

    logo

--- src/components/AboutHero.vue
+++ src/components/AboutHero.vue
@@ -8,7 +8,7 @@
             <img class="hidden md:block md:h-full w-full object-cover object-center" src="../assets/images/hvem_er_vi_carlsens_kvarterstor.jpg"/>
             <img class=" md:hidden h-[60vh] md:h-full w-full object-cover object-center" src="../assets/images/hvem_er_vi_carlsens_kvarter_mobil.jpg"/>
             <div class=" bg-cream-primary text-center p-2 absolute bottom-10 mx-4">
-                <img class="hidden" src="" alt="carlsens logo">
+                <img class="absolute top-[-4px] left-1/2 transform -translate-x-1/2 -translate-y-1/2" src="../assets/images/logos/ck_logo_round.svg" alt="carlsens kvarters runde logo">
                 <div class="w-full h-full border-solid border-[1px] border-border-black px-6 py-5 flex flex-col justify-center items-center md:min-w-[578px]">
                     <h1>HVEM ER VI</h1>
                 </div>           
--- src/components/ButtonPrimary.vue
+++ src/components/ButtonPrimary.vue
@@ -5,8 +5,8 @@
 </script>
 
 <template>
-    <a href="https://m.me/Carlsenskvarter" target="_blank">
-        <div class=" self-center bg-cream-primary p-[5px] mt-6 md:mt-3 md:self-start lg:absolute lg:right-5 lg:bottom-5 lg:z-10 w-fit lg:self-end duration-100 hover:scale-95">
+    <a class="w-fit self-center" href="https://m.me/Carlsenskvarter" target="_blank">
+        <div class=" bg-cream-primary p-[5px] mt-6 md:mt-3 md:self-start lg:absolute lg:right-5 lg:bottom-5 lg:z-10 w-fit lg:self-end duration-100 hover:scale-95">
             <button class="border-solid border-[1px] border-border-black px-5 py-2 min-w-[160px]">{{ buttonText }}</button>
         </div>
     </a>
