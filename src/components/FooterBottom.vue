899ceed52f9e53653124c1acecd39b0e20e6bfa7
IBATMANI <abasinsafi@hotmail.com>
2023-05-19 11:03:13 +0200

    some icons i footer

--- src/components/FooterBottom.vue
+++ src/components/FooterBottom.vue
@@ -38,7 +38,13 @@
 
         <div class="flex flex-col justify-center pb-8 max-w-[330px] mx-auto md:max-w-content-width">
             <div class="bg-border-white w-full md:max-w-content-width h-[2px]"></div>
-            <p class="mt-4 text-lg">© 2023 Carlsens Kvarter</p>
+            <div class="flex flex-row justify-between mb-4 md:flex-row  md:mb-3 items-center">
+                <p class="mt-4 text-lg">© 2023 Carlsens Kvarter</p>
+                <div class="flex flex-row mt-2">
+                <img class="justify-item-start max-w-[25px] h-[25px] mr-1 md:mr-2 md:max-w-[30px] md:h-[26px] md:my-1 md:justify-item-center" src="../assets/images/kontakt/fb.svg" alt="facebook icon">
+                <img class="justify-item-start max-w-[25px] h-[25px] mr-1 md:mr-2 md:max-w-[30px] md:h-[30px] mt:my-1 md:justify-item-center" src="../assets/images/kontakt/insta.svg" alt="instagram icon">
+                </div>
+            </div>
         </div>
 
     </footer>  
