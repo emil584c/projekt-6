c0375981ed308c6cca7b149e7d4851e46e117aba
emil584c <105641442+emil584c@users.noreply.github.com>
2023-05-21 16:56:27 +0200

    contact margin mobile fix

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -15,9 +15,9 @@ import buttonPrimary from './ButtonPrimary.vue'
                             <p class=" text-base font-medium">ALLE BESTILLINGER SKER OVER MESSENGER</p>
                             <buttonPrimary buttonText="BESTIL"/>
                     </div>
-                    </div>
-                    <img src="../assets/images/bestillinger_carlsens_kvarter.jpg" class=" w-mobilebox-width h-mobilebox-height bg-center md:w-box-width object-cover" alt="billede af tjener der hælder øl op">
                 </div>
+                <img src="../assets/images/bestillinger_carlsens_kvarter.jpg" class=" w-mobilebox-width h-mobilebox-height bg-center md:w-box-width object-cover my-4 md:my-0" alt="billede af tjener der hælder øl op">
+            </div>
 
             <div class="flex flex-col md:flex-row md:justify-between md:my-[40px]">
                 <div class=" bg-brown-primary w-mobilebox-width h-mobilelongbox-height md:h-box-width bg-center flex flex-col justify-center p-[10px] md:w-full md:max-w-longbox-width">               
@@ -26,18 +26,18 @@ import buttonPrimary from './ButtonPrimary.vue'
                             <p class=" mb-3 max-w-[386px] md:max-w-full">Hvis du har spørgsmål eller forespørgsler udover bestilling af ølsmagning, håndmadder eller arrangement, kan du kontakte os på følgende muligheder.</p>
                             <p class=" text-base font-medium">BORDRESERVATION ER IKKE MULIGT.</p>
                             <div class="bg-border-white w-full md:max-w-content-width h-[1px] my-2"></div>
-                        <div class="flex flex-col md:flex-row md:justify-between">
-                            <div class="flex flex-row mb-4 md:flex-col  items-center">
-                                <img class="justify-item-start max-w-[25px] h-[25px] mr-1 md:mr-0 md:max-w-[40px] md:h-[40px] md:my-7 md:justify-item-center" src="../assets/images/kontakt/mail.svg" alt="mail icon">
-                                <p class="mb-1 md:mb-3 ">carlsens@carlsens.dk</p>
+                        <div class="flex flex-col lg:flex-row lg:justify-between">
+                            <div class="flex flex-row mb-4 lg:flex-col  items-center">
+                                <img class="justify-item-start max-w-[25px] h-[25px] mr-1 lg:mr-0 lg:max-w-[40px] lg:h-[40px] lg:my-7 lg:justify-item-center" src="../assets/images/kontakt/mail.svg" alt="mail icon">
+                                <p class="mb-1 lg:mb-3 ">carlsens@carlsens.dk</p>
                             </div>
-                            <div class="flex flex-row mb-4 md:flex-col  items-center">
-                                <img class="justify-item-start max-w-[25px] mr-1 md:mr-0 md:max-w-[40px] md:my-7 md:justify-item-center" src="../assets/images/kontakt/tele.svg" alt="telephone icon">
+                            <div class="flex flex-row mb-4 lg:flex-col  items-center">
+                                <img class="justify-item-start max-w-[25px] mr-1 lg:mr-0 lg:max-w-[40px] lg:my-7 lg:justify-item-center" src="../assets/images/kontakt/tele.svg" alt="telephone icon">
                                 <p>+45 63 11 01 10</p>
                             </div>
-                            <div class="flex flex-row mb-4 md:flex-col items-center">
-                                <img class="justify-item-start max-w-[25px] h-[40px] pl-2 pr-1 mr-1 md:mr-0 md:max-w-[40px] md:h-[40px] md:my-7 md:justify-item-center" src="../assets/images/kontakt/pin.svg" alt="pin icon">
-                                <p class="mb-1 md:mb-3 ">Hunderupvej 19, 5000 Odense C</p>
+                            <div class="flex flex-row mb-4 lg:flex-col items-center">
+                                <img class="justify-item-start max-w-[25px] h-[40px] pl-2 pr-1 mr-1 lg:mr-0 lg:max-w-[40px] lg:h-[40px] lg:my-7 lg:justify-item-center" src="../assets/images/kontakt/pin.svg" alt="pin icon">
+                                <p class="mb-1 lg:mb-3 ">Hunderupvej 19, 5000 Odense C</p>
                             </div>
                         </div>
                     </div>
