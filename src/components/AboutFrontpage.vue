daff1ce5a3c40542eaa2eabbab6f0e409310579a
emil584c <105641442+emil584c@users.noreply.github.com>
2023-05-21 16:26:00 +0200

    about us button link

--- src/components/AboutFrontpage.vue
+++ src/components/AboutFrontpage.vue
@@ -9,12 +9,13 @@
         </div>
         <div class=" px-4 md:px-0 md:max-w-content-width mx-auto w-full justify-center">
             <div class="flex flex-col gap-4 md:gap-10">
-
                 <div class="flex flex-col gap-4 md:gap-10 items-center justify-center md:items-stretch md:flex-row md:justify-between">
                     <div class="bg-[url('../assets/images/holdet_bag_carlsen_kvarter.jpg')] bg-cover w-mobilebox-width h-mobilebox-width md:h-mobilebox-height bg-center flex justify-center items-end p-10 md:max-w-longbox-width md:w-full md:justify-end md:p-8">
-                        <div class=" bg-cream-primary p-[5px] relative z-10 w-fit hover:scale-95 duration-100">
-                            <button class="border-solid border-[1px] border-border-black px-5 py-2">LÆR OS AT KENDE</button>
-                        </div>
+                        <router-link to="/about">
+                            <div class=" bg-cream-primary p-[5px] relative z-10 w-fit hover:scale-95 duration-100">
+                                <button class="border-solid border-[1px] border-border-black px-5 py-2">LÆR OS AT KENDE</button>
+                            </div>
+                        </router-link>
                     </div>
                     <div class="bg-[url('../assets/images/aabningstider_carlsens_kvarter.jpg')] bg-cover w-mobilebox-width h-mobilebox-width md:h-mobilebox-height bg-center flex flex-col justify-center gap-6 pl-5 md:w-box-width">
                         <ul class="flex flex-col gap-1">
