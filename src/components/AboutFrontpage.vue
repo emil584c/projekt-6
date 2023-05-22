e10142593e1562450c49fa697d19ea69ad9145e6
emil584c <105641442+emil584c@users.noreply.github.com>
2023-05-22 11:46:45 +0200

    about image fix

--- src/components/AboutFrontpage.vue
+++ src/components/AboutFrontpage.vue
@@ -9,7 +9,7 @@
         </div>
         <div class=" px-4 md:px-0 md:max-w-content-width mx-auto w-full justify-center">
             <div class="flex flex-col gap-4 md:gap-10">
-                <div class="flex flex-col gap-4 md:gap-10 items-center justify-center md:items-stretch md:flex-row md:justify-between overflow-hidden">
+                <div class="flex flex-col gap-4 md:gap-10 items-center justify-center md:items-stretch md:flex-row md:justify-between max-w-[330px] mx-auto md:mx-0 md:max-w-content-width overflow-hidden">
                     <div class="bg-[url('../assets/images/holdet_bag_carlsen_kvarter.jpg')] bg-cover w-mobilebox-width h-mobilebox-width md:h-mobilebox-height bg-center flex justify-center items-end p-10 md:max-w-longbox-width md:w-full md:justify-end md:p-8">
                         <router-link to="/about">
                             <div class=" bg-cream-primary p-[5px] relative z-10 w-fit hover:scale-95 duration-100">
